dce84cf819ec9760ce0cd3387bfb021af536375c
jinwon27 <wlsdnjs4284@naver.com>
2022-08-09 22:35:28 +0900

    A+B -5

---
+++ beakjoon/Q_10952.java
@@ -0,0 +1,22 @@
+package beekjoon;
+
+import java.util.Scanner;
+
+public class Q_10952 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			if(A==0 && B==0) {
+				sc.close();
+				break;
+			}
+			System.out.println(A+B);
+		}
+	}
+}
