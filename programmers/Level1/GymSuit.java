234b0d99361f2f53f66aca45108ce3ed47940e9b
jinwon27 <wlsdnjs4284@naver.com>
2022-08-19 10:09:13 +0900

    :lock:

---
+++ programmers/Level1/GymSuit.java
@@ -0,0 +1,30 @@
+
+package programmers;
+
+import java.util.*;
+
+public class GymSuit {
+	public int solution(int n, int[] lost, int[] reserve) {
+		
+		int answer = n;
+		int[] student = new int[n];
+		
+		Arrays.fill(student, 1);
+		for(int i : lost)
+			student[i - 1] -= 1;
+		for(int i : reserve)
+			student[i - 1] += 1;
+		for(int i = 0; i < student.length; i++ ) {
+			if(student[i] == 0) {
+				if(i > 0 && student[i - 1] == 2)
+					student[i - 1] -= 1;
+				else if(i < n - 1 && student[i + 1] == 2)
+					student[i + 1] -= 1;
+				else
+					answer--;
+			}
+		}		
+		return answer;
+		
+	}
+}
